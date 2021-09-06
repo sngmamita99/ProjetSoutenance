cbabb92d6fe800b32fd8c60d03ae07f23e199b10
sngmamita99 <86856771+sngmamita99@users.noreply.github.com>
2021-09-06 07:09:32 +0200

    activer et desactiver

---
+++ activer.php
@@ -0,0 +1,21 @@
+<?php 
+    $cd=$_GET['code'];
+	$connect=new PDO("mysql:host=localhost;port=3306;dbname=ecivil","root","");
+    if($connect){                 
+	    $req="UPDATE users SET etat='1' WHERE iduser='$cd'";
+	    $res=$connect->query($req);
+		$row=$res->fetch();
+		if($row['profil']=="agent"){
+		    header("Location:agent.php");
+	    }
+        else if($row['profil']=="officier"){
+		    header("Location:officier.php");
+	    } 
+ 		else if($row['profil']=="citoyen"){
+		    header("Location:citoyen.php");
+	    }
+		else{
+		    header("Location:livreur.php");
+	    }
+	}	
+?>
