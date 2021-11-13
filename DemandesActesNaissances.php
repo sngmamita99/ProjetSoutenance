ff1e98c4733b74b97b23ed2acad03b5fa082643b
fatousow1997 <86850873+fatousow1997@users.noreply.github.com>
2021-11-13 23:46:59 +0100

    changement definitive de l'index

--- DemandesActesNaissances.php
+++ DemandesActesNaissances.php
@@ -1,40 +1,208 @@
+
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8"/>
-		<title>demande d'acte de naissance</title>
-	</head>
-	<body>
-	<form method="POST" action="">
-		<p>
-			<label for="nom">Nom</label>
-			<input type="text" name="nom" id="nom">
-		</p>
-		<p>
-			<label for="prenom">Prenom</label>
-			<input type="text" name="prenom" id="prenom">
-		</p>
-		<p>
-			<label for="numDeRegistre">Numero De Registre</label>
-			<input type="int" name="numDeRegistre" id="numDeRegistre">
-		</p>
-		<p>
-			<label for="annee_registre">Annee de Registre</label>
-			<input type="text" name="annee_registre" id="annee_registre">
-		</p>
-		<p>
-			<label >Methode de paiement</label>
-			<select name="methode_paiement">
-				<option value="En ligne">En ligne</option>
-				<option value="Sur place">Sur place</option>
-			<select>
-			
-		</p>
-		<p>
-		<input type="submit" Value="Envoyer"/>
-		</P>
-	</form>
-	</body>
+
+<head>
+    <title>Demande d'acte d'état civils</title>
+    <meta charset="utf-8">
+<!--Import materialize.min.css-->
+<link type="text/css" rel="stylesheet" href="css1/materialize.min.css" media="screen,projection" />
+<link type="text/css" rel="stylesheet" href="css1/icones.css" media="screen,projection" />
+<link type="text/css" rel="stylesheet" href="css1/formulaire.css" media="screen,projection" />
+<style type="text/css">
+body {
+    background-color: #eff1f2;
+}
+</style>
+<!--Let browser know website is optimized for mobile-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<!--Import jQuery before materialize.js-->
+<script type="text/javascript" src="js1/jquery.min.js"></script>
+<script type="text/javascript" src="js1/materialize.min.js"></script>
+
+
+<script type="text/javascript">
+// Set timeout variables.
+var timoutNow = 900000;
+var logoutUrl = 'deconnexion.php?session_expired=1';
+var timeoutTimer;
+// Start timers.
+function StartTimers() {
+    timeoutTimer = setTimeout("IdleTimeout()", timoutNow);
+}
+// Reset timers.
+function ResetTimers() {
+    console.log('reset');
+    clearTimeout(timeoutTimer);
+    StartTimers();
+    $('#idle_warning').hide();
+}
+
+// Show idle timeout warning dialog.
+function IdleWarning() {
+    $('#idle_warning').show();
+}
+
+// Logout the user.
+function IdleTimeout() {
+
+    $('#timeoutModal').modal('open');
+    $('#idle_warning').show();
+}
+
+$(document).ready(function() {
+    $('.modal').modal({
+        dismissible: false,
+        opacity: 0.6, // Opacity of modal background
+        in_duration: 300, // Transition in duration
+        out_duration: 200, // Transition out duration
+        preventScrolling: false,
+        onCloseEnd: function() {
+            window.location = "deconnexion.php";
+        }
+    });
+    StartTimers();
+    $(document).on('mousemove scroll keyup keypress mousedown mouseup mouseover', function() {
+        ResetTimers();
+    });
+});
+
+$(window).on('load', function() {
+
+    $(window).on('mousemove scroll keyup keypress mousedown mouseup mouseover', function() {
+        ResetTimers();
+    });
+
+});
+</script>
+</head>
+
+<body >
+   
+    <div class="container white" >
+        <div class="row">
+            <form class="col s12 m12 l12" method="POST" action="e_demande_acte_trmnt.php">
+                <input type="number" name="s" hidden="">
+                <h3 class="center">Formulaire de demande d'acte de Naissance</h3>
+                <div class="row">
+                    
+                    <div class=" input-field col s6 m4 l4">
+                        <select class="browser-default registre" required="" name="registre" id="registre">
+                            <option value="" disabled selected>Choisissez le registre</option>
+                            <option value="Registre des deces">Registre des deces</option>
+                            <option value="Registre des mariages">Registre des mariages</option>
+                            <option value="Registre des naissances">Registre des naissances</option>
+                        </select>
+                    </div>
+                    <div class="naissances input-field col s6 m4 l4 " id="naissances">
+                        <select class="browser-default acte" name="naissances">
+                            <option value="" disabled selected>Choisissez l'acte naissance</option>
+                            <option value="Extrait de naissance">Extrait de naissance</option>
+                            <option value="Copie littérale de naissance">Copie littérale de naissance</option>
+                        </select>
+                    </div>
+                    <div class="deces input-field col s6 m4 l4 " id="deces">
+                        <select class="browser-default acte" name="deces">
+                            <option value="" disabled selected>Choisissez l'acte de deces</option>
+                            <option value="Certificat de deces">Certificat de deces</option>
+                            <option value="Copie littérale de deces">Copie littérale de deces</option>
+                        </select>
+                    </div>
+                    <div class="mariages input-field col s6 m4 l4 " , id="mariages">
+                        <select class="browser-default acte" name="mariages">
+                            <option value="" disabled selected>Choisissez l'acte de mariage</option>
+                            <option value="Certificat de mariage">Certificat de mariage</option>
+                            <option value="Copie littérale de mariage">Copie littérale de mariage</option>
+                            <option value="Attestation de changement d’option">Attestation de changement d’option
+                            </option>
+                        </select>
+                    </div>
+
+                </div>
+                <div class="row">
+                    <div class="input-field col s6 m2 l2">
+                        <input type="number" required min="1980" max="2021" name="annee_acte"
+                            id="annee_acte">
+                        <label for="annee_acte">Année du registre</label>
+                    </div>
+                    <div class="input-field col s6 m2 l2">
+                        <input type="number" required min="1" name="num_acte" id="num_acte">
+                        <label for="num_acte">N° du registre</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s6 m5 l5">
+                        <input type="text" required name="prenom" id="prenon">
+                        <label for="preno">Prénom figurant sur l'acte</label>
+                    </div>
+                    <div class="input-field col s6 m5 l5">
+                        <input type="text" required name="nom" id="nom">
+                        <label for="nom">Nom figurant sur l'acte</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s6 m3 l3">
+                        <input type="number" required min="1" value="1" max="15" name="nbr_copies" id="nbr_copies">
+                        <label for="nbr_copies">Nombre de copies de l'acte</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col s2 offset-s8 input-field">
+                        <input class="btn white-text blue darken-4" type="submit" name="enregistrer"
+                            value="Enregistrer">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+<script type="text/javascript">
+$(document).ready(function() {
+    $('#naissances').addClass('hide');
+    $('#mariages').addClass('hide');
+    $('#deces').addClass('hide');
+    $('input').addClass('hide');
+    $('label').addClass('hide');
+
+    function select_acte() {
+        var registre = $('.registre').val();
+        if (registre == "Registre des deces") {
+            $('#naissances').addClass('hide');
+            $('#mariages').addClass('hide');
+            $('#deces').removeClass('hide');
+        } else if (registre == "Registre des naissances") {
+            $('#deces').addClass('hide');
+            $('#mariages').addClass('hide');
+            $('#naissances').removeClass('hide');
+        } else if (registre == "Registre des mariages") {
+            $('#naissances').addClass('hide');
+            $('#deces').addClass('hide');
+            $('#mariages').removeClass('hide');
+        }
+        $('input').removeClass('hide');
+        $('label').removeClass('hide');
+
+    }
+
+
+    $('.registre').change(function() {
+        select_acte();
+    });
+    $('form').submit(function() {
+        if (!confirm('Voulez-vous confirmer l\'enregistrement de cette demande d\'acte ?')) {
+            return false;
+        }
+    })
+
+})
+</script>
+<style type="text/css">
+body {
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+}
+</style>
+
 </html>
-<?php
-?>
--- index.php
+++ index.php
@@ -1,40 +1,215 @@
-<?php
-session_start();
-?>
+
 <!DOCTYPE html>
-<html lang="en">
-    <head> 
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="accueil.css?t=<?php echo time();?>">	
-    <title>Index</title>
-	<style>
+<html>
+
+<head>
+    <title>Accueil</title>
+    <meta charset="utf-8">
+<!--Import materialize.min.css-->
+<link type="text/css" rel="stylesheet" href="css1/materialize.min.css" media="screen,projection" />
+<link type="text/css" rel="stylesheet" href="css1/icones.css" media="screen,projection" />
+<link type="text/css" rel="stylesheet" href="css1/formulaire.css" media="screen,projection" />
+<style type="text/css">
+body {
+    background-color: #eff1f2;
+}
+</style>
+<!--Let browser know website is optimized for mobile-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<!--Import jQuery before materialize.js-->
+<script type="text/javascript" src="js1/jquery.min.js"></script>
+<script type="text/javascript" src="js1/materialize.min.js"></script>
+
+
+<div class="modal modal" id="timeoutModal">
+    <div class="modal-content">
+        <h4 class="center">Timeout</h4>
+        <h5>
+            Vous session a été déconnecté.<br>
+            Veillez vous reconnectez.
+        </h5>
+    </div>
+    <div class="modal-footer">
+        <a class="modal-close btn-flat waves-effect blue">Connexion</a>
+    </div>
+</div>
+
+<script type="text/javascript">
+// Set timeout variables.
+var timoutNow = 900000;
+var logoutUrl = 'deconnexion.php?session_expired=1';
+var timeoutTimer;
+// Start timers.
+function StartTimers() {
+    timeoutTimer = setTimeout("IdleTimeout()", timoutNow);
+}
+// Reset timers.
+function ResetTimers() {
+    console.log('reset');
+    clearTimeout(timeoutTimer);
+    StartTimers();
+    $('#idle_warning').hide();
+}
+
+// Show idle timeout warning dialog.
+function IdleWarning() {
+    $('#idle_warning').show();
+}
+
+// Logout the user.
+function IdleTimeout() {
+
+    $('#timeoutModal').modal('open');
+    $('#idle_warning').show();
+}
+
+$(document).ready(function() {
+    $('.modal').modal({
+        dismissible: false,
+        opacity: 0.6, // Opacity of modal background
+        in_duration: 300, // Transition in duration
+        out_duration: 200, // Transition out duration
+        preventScrolling: false,
+        onCloseEnd: function() {
+            window.location = "deconnexion.php";
+        }
+    });
+    StartTimers();
+    $(document).on('mousemove scroll keyup keypress mousedown mouseup mouseover', function() {
+        ResetTimers();
+    });
+});
+
+$(window).on('load', function() {
+
+    $(window).on('mousemove scroll keyup keypress mousedown mouseup mouseover', function() {
+        ResetTimers();
+    });
+
+});
+</script>
+</head>
+
+<body style="background-color: #eff1f2;">
+    <nav>
+        <div class="nav-wrapper">
+            <a href="index.php" class="brand-logo">&nbsp&nbsp&nbspRegistre+</a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="Connexion.php"><img id="fa" src="fa.png" alt="se connecter"></a></li>
+				
+            </ul>
+        </div>
+    </nav>
+    <div class="row">
+        <div class="slider ">
+            <ul class="slides">
+                <li>
+                    <img src="https://cdn.pixabay.com/photo/2016/11/23/14/37/blur-1853262_1280.jpg"> <!-- random image -->
+                    <div class="caption left-align ">
+                        <h3 style="color:white" class="">Persistance des données</h3>
+                        <h5 class="light white-text text-lighten-3 blue-grey darken-1" > Introduction et sauvegarde des données d'un acte d'état civil (Naissance, Mariage, Décès), anciens ou en cours de constitution</h5>
+                    </div>
+                </li>
+                <li>
+                    <img src="https://cdn.pixabay.com/photo/2017/06/28/10/06/binary-2450153_1280.jpg"> <!-- random image -->
+                    <div class="caption left-align">
+                        <h3 >Utilisation ergonomique</h3>
+                        <h5 class="light white-text text-lighten-3 blue-grey darken-1" >Faciliter le travail de saisie des données en saisissant automatiquement les informations du
+                centre d’état civil</h5>
+                    </div>
+                </li>
+                <li>
+                    <img src="https://cdn.pixabay.com/photo/2015/06/25/17/21/smart-watch-821557_1280.jpg"> 
+                    <div class="caption left-align">
+                        <h3>Gain de temps</h3>
+                        <h5 class="light grey-text text-lighten-3">	Rendre efficace le processus de déclaration de naissance</h5>
+                    </div>
+                </li>
+                <li>
+                    <img src="https://cdn.pixabay.com/photo/2020/07/08/04/12/work-5382501_1280.jpg"> <!-- random image -->
+                    <div class="caption center-align">
+                        <h3>Vue d'ensemeble des données</h3>
+                        <h5 class="light white-text text-lighten-3 blue-grey darken-1" >Visualiser sélectivement les données en fonction de leur statut (en attente de validation, validé, transféré, confirmé)</h5>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+    
+    <div class="row">
+        <h5 class="col s6"></h5>
+    </div>
+</body>
+<style type="text/css">
+nav {
+  background-color: #00695c  ; 
+}
+body {
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+    background-color: #999;
+}
+
+#preloader {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #fefefe;
+    z-index: 99;
+    height: 100%;
+}
+
+#status {
+    width: 200px;
+    height: 200px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    background-image: url(css/images/Spinner.gif);
+    background-repeat: no-repeat;
+    background-position: center;
+    margin: -100px 0 0 -100px;
+}
+#fa
+{
 	
-	a
-	{
-		text-decoration:none;
-		color:white;
-		font-weight:bold;
-		font-size:15px;
-		
-	}
-	button
-	{
-		margin-left:88%;
-		background:green;
-		height:30px;
-		border-radius:5px 5px;
-		border-color:green;
-	}
-		</style>
-    </head>
-    <body>
-	<button><a href="Connexion.php">Se Connecter</a></button>
-        <section id="part1">
-		<p><span>La conservation numérique</span><br/> 
-		de l’état civil sénégalais,un moyen <br/>d’une démocratisation de l’accès à
-		l’information<br/> dans une ville intelligente 
-		</p>
-        </section>
-    </body>
+	width:30px;
+	height:30px;
+}
+
+</style>
+<script type="text/javascript">
+$(document).ready(function() {
+    $('.slider').slider({
+        indicators: false,
+        height: 500
+    });
+
+    // will first fade out the loading animation
+    //jQuery("#status").fadeOut();
+    jQuery("#status").delay(400).fadeOut("slow");
+    // will fade out the whole DIV that covers the website.
+    jQuery("#preloader").delay(400).fadeOut("slow");
+
+    function etat_demande() {
+        var r = $('input').val();
+        $.ajax({
+            type: 'POST',
+            url: 'etat_demande_ajax.php',
+            data: 'r=' + r,
+            success: function(html) {
+                $('.recherche').html(html);
+            }
+        });
+    }
+    $('.btn_recherche').click(function() {
+        etat_demande();
+    });
+})
+</script>
+
 </html>
