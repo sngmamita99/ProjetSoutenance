80991beb3e32b2b529ff862b85c062f863fc3e9e
fatousow1997 <86850873+fatousow1997@users.noreply.github.com>
2021-11-14 03:12:45 +0100

    nouvelle designe

--- DemandeDecNaiss.php
+++ DemandeDecNaiss.php
@@ -86,7 +86,7 @@ $(window).on('load', function() {
 	<body id="debut" >
 		<!--Dropdown structure demande d'acte -->
 <ul id="demande_acte" class="dropdown-content">
-  <li> <a href="l_demande_acte.php"> Naissance</a></li>
+  <li> <a href="dem.php"> Naissance</a></li>
   <li><a href="l_demande_acte_confirme.php">Mariage</a></li>
     <li><a href="l_demande_acte_confirme.php">Déces</a></li>
 </ul>
@@ -134,7 +134,7 @@ $(window).on('load', function() {
                       </a>
         </li>
 		 <li>
-          <a href="DemandeDecNaiss.php" class="dropdown-trigger" data-beloworigin="true" data-target="declaration">
+          <a href="l_demande_acte.php" class="dropdown-trigger" data-beloworigin="true" data-target="declaration">
             Demandes de déclaration
             </a>
         </li>
@@ -202,9 +202,80 @@ alignment: 'right'
 		
 </style>
 <body>
-mettres des couroussels
+	 <!-- Pièces Justificatives -->
+					    
+					  <form method="POST" action="traitementDeclarationNaiss.php" enctype="multipart/form-data">
+					  <div class="row" id="doc">
+								<div class="file-field input-field col s12">
+									<div class="btn white darken-4">
+										<span>Lien de Parenté</span>
+										
+									</div>
+									<div class="file-path-wrapper">
+										<select name="LienParente" required id="sexe_enfant" class="browser-default">
+											<option value="" selected disabled >--parente--</option>
+											<option value="pere"  >pere</option>
+											<option value="mere" >mere</option>
+											<option value="proche" >proche</option>
+										</select>
+									</div>
+								</div>
+							</div>
+					  
+					      	<div class="row" id="doc">
+								<div class="file-field input-field col s12">
+									<div class="btn white darken-4">
+										<span >Pièce d’identités</span>
+										<input type="file" accept="image/*" name="monFichier1" class=" pi" >
+									</div>
+									<div class="file-path-wrapper">
+										<input class="file-path validate pi" placeholder="Carte National d’identité du pére"  type="text" >
+									</div>
+								</div>
+							</div>
+							<div class="row" id="doc">
+								<div class="file-field input-field col s12">
+									<div class="btn white darken-4">
+										<span >Pièce d’identité </span>
+										<input type="file" accept="image/*" name="monFichier2" class=" domicle" >
+									</div>
+									<div class="file-path-wrapper">
+										<input class="file-path validate domicle" placeholder="Carte National d’identité du pére"  type="text" >
+									</div>
+								</div>
+							</div>
+							<div class="row" id="doc">
+								<div class="file-field input-field col s12">
+									<div class="btn white darken-4">
+										<span >Sélectionner</span>
+										<input type="file" accept="image/*" name="monFichier3" class=" fichier_naissance">
+									</div>
+									<div class="file-path-wrapper">
+										<input class="file-path validate fichier_naissance" placeholder="Attestation de naissance de l’hôpital / Clinique / Sanitaire"  type="text" >
+									</div>
+								</div>
+							</div>
+					      </div>
+						  <div class="col s3 m1 offset-m1 offset-s6 input-field">
+						<input class="btn valider" type="submit" style="background-color:#00695c" name="enregsitrer" value="Envoyer">
+					</div>
+					    </form>
+
 </body>
 	<style type="text/css">
+	span{
+		color:black;
+		font-weight:bold
+		
+	}
+	form
+	{
+		background:white;
+		width:50%;
+		margin-left:25%;
+		
+		
+	}
 
 		body
 		{
@@ -212,6 +283,7 @@ mettres des couroussels
 			background-repeat:  no-repeat;
 			background-attachment: fixed;
 			background-size:  cover;
+			background-image:url("images/nai.jpg");
 
 		}
 		table{
--- traitementDeclarationNaiss.php
+++ traitementDeclarationNaiss.php
@@ -1,56 +1,84 @@
 <?php
  require_once("SessionError.php");
-session_start();
+
 $SESSION=new Session();
 $idConnected=$_SESSION['idConnected'];
-	// $date_declaration= date('dmYHis');
-	$date_declaration = date("Y-m-d");
-	
-	// $pD=$_POST['prenom'];
-	// $nD=$_POST['nom'];
-	$lP=$_POST['LienParente'];
-	// $libelle=$_POST['libelle'];
-	
+$date_declaration = date("Y-m-d");
+$lP=$_POST['LienParente'];
+$k=0;
 	$bdd = new PDO ("mysql:host=localhost;dbname=ecivil","root", "");
 	if($bdd)
 	{
 		
-		$req="insert into declarationnaissance (numCompte,lienDeParente,date_declaration) values (?,?,?)";
+		$req="insert into declarationnaissance (numCompte,lienDeParente,etat,date_declaration) values (?,?,?,?)";
 		$stmt=$bdd->prepare($req);
 		$stmt->bindParam(1, $idConnected);
-		// $stmt->bindParam(2, $nD);
-		// $stmt->bindParam(3,$pD);
 		$stmt->bindParam(2, $lP);
-		$stmt->bindParam(3,$date_declaration);
+		$stmt->bindParam(3,$k);
+		$stmt->bindParam(4,$date_declaration);
 		$stmt->execute();
-		$numDecla=$bdd->lastInsertId();//lastInsertId si je veux savoir l'identifiant de la derniere ligne dans la base de donne   
-				if(!empty($_FILES['monFichier']))
+		$numDecla=$bdd->lastInsertId();
+				if(!empty($_FILES['monFichier1']))
 					{
-						$photos = array();
+						
+						
 						$h=date('dmYHis',time());
-						$photos=$_FILES['monFichier'];
-						$nb = count($photos['name']);
-						for($i=0;$i<$nb;$i++)
+						$photos=$_FILES['monFichier1'];
+						$dest=$h.'_'.$photos['name'];
+						$chemin ='photos/'.$dest;
+						$images=move_uploaded_file($photos['tmp_name'],$chemin);
+						if($images)
 						{
-								// $extension =  pathinfo($photos['name'][$i])['extension'];
-								// $chemin ='photos/'.basename($_POST['nomProduit'].($i+1).'.'.$extension);
-								$dest=$h.'_'.$photos['name'][$i];
-								 $chemin ='photos/'.$dest;
-								$images=move_uploaded_file($photos['tmp_name'][$i],$chemin);
-								if($images)
-								{
-									$req2 = $bdd->prepare("INSERT into justification(numDeclaration,url) values (?,?)");
-									$res1 = $req2->execute(array($numDecla,$chemin));
-								}
-								else
-								{
-									echo"transfert non reussi";
-								}
+							$req2 = $bdd->prepare("INSERT into justification(numDeclaration,url) values (?,?)");
+							$res1 = $req2->execute(array($numDecla,$chemin));
 						}
+						else
+						{
+							echo"transfert non ok";
+						}
+					}
+					if(!empty($_FILES['monFichier2']))
+					{
+						$h=date('dmYHis',time());
+						$photos=$_FILES['monFichier2'];
+						$dest=$h.'_'.$photos['name'];
+						$chemin ='photos/'.$dest;
+						$images=move_uploaded_file($photos['tmp_name'],$chemin);
+						if($images)
+						{
+							$req2 = $bdd->prepare("INSERT into justification(numDeclaration,url) values (?,?)");
+							$res1 = $req2->execute(array($numDecla,$chemin));
+						}
+						else
+						{
+							echo"transfert non ok";
+						}
+					}
 			
-					}	
-					$SESSION->setFlash("Votre demande 	a été envoyée avec succés ! Vous allez recevoir la date de votre RV pour la declaration",'success');
-							header("Location:acceuilCitoyen.php");
+			if(!empty($_FILES['monFichier3']))
+					{
+						
+						
+						$h=date('dmYHis',time());
+						$photos=$_FILES['monFichier3'];
+						$dest=$h.'_'.$photos['name'];
+						$chemin ='photos/'.$dest;
+						$images=move_uploaded_file($photos['tmp_name'],$chemin);
+						if($images)
+						{
+							$req2 = $bdd->prepare("INSERT into justification(numDeclaration,url) values (?,?)");
+							$res1 = $req2->execute(array($numDecla,$chemin));
+						}
+						else
+						{
+							echo"transfert non ok";
+						}
+					}
+			
+						
+					// echo"ok";
+					// $SESSION->setFlash("Votre demande 	a été envoyée avec succés ! Vous allez recevoir la date de votre RV pour la declaration",'success');
+							// header("Location:acceuilCitoyen.php");
 		
 	
 	}
