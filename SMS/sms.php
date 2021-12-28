783f39ab24e33db9ab600b304d7679514121cceb
fatousow1997 <86850873+fatousow1997@users.noreply.github.com>
2021-12-28 17:46:56 +0100

    ok

--- SMS/sms.php
+++ SMS/sms.php
@@ -1,3 +1,9 @@
 <?php
-requir_once("autoload.php");
+require_once("vendor/autoload.php");
+$MessageBird=new \MessageBird\Client('Mg3D9i2of1UHD2LyIPREVHvCT');
+$Message=new \MessageBird\Objects\Message();
+$Message->originator="Ecivil";
+$Message->recipients =array("+221772267403");
+$Message->body="Salut fatou ";
+print_r(json_encode($MessageBird->messages->create($Message)));
 ?>
